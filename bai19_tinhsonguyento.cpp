15fce92bcfd907ba078f8e62b983b4ce871edd33
MThiện CNTT <thanhsooon1111@gmail.com>
2024-11-25 15:17:26 +0700

    Update4 bai19_tinhsonguyento.cpp

--- bai19_tinhsonguyento.cpp
+++ bai19_tinhsonguyento.cpp
@@ -20,51 +20,26 @@ bool(kiemtraSNT(int s))
 		return false;
 	}
 }
+bool kiemtraSCP(int x) {
+	if (sqrt(x) == (int)sqrt(x)) return true;
+	return false;
+
+}
 int main() {
 	
 	cout << "KIEM TRA SO CO PHAI SO NGUYEN TO KHONG\n";
-	int so;
-	cout << "nhap so: "; cin >> so;
-		for (int y = 1; y <= so; y++)
+	int n;
+	cout << "nhap so: "; cin >> n;
+		for (int y = 1; y <= n; y++)
 		{
 			if (kiemtraSNT(y) == true) cout << y << " ";
 		
-	}/*
-	for (int i = 1; i <= n; i++)
-	{
-=======
-int main() {
-	int n;
-	int dem = 0;
-	cout << "nhap so:\n"; cin >> n;
-	for (int y = 1; y <= n; y++) {
-		dem = 0;
-		for (int i = 1; i <= y; i++)
+	}
+		for (int i = 1; i < 100; i++)
 		{
-			if (y % i == 0) {
-				dem++;
-
-			}
+			if (sqrt(i) == (int)sqrt(i)) cout << i << "la so chinh phuong\n";
 		}
-		if (dem == 2) {
-			cout << y << " ";
-		}
-	}/*
-	for (int i = 1; i <= n; i++)
-	{
->>>>>>> 5f9526fb0a18ef0bf6e99be886a6f638ea227379
-		if (n % i == 0) {
-			dem++;
-
-		}
-	}
-	//cout << "so dem =" << dem << endl;
-	if (dem == 2) {
-		cout << "So " << n << " la so nguyen to\n";
-	}
-	else{
-		cout << "So " << n << " khong phai la so nguyen to\n";
-	}*/
+		
 	return 0;
 
 }
