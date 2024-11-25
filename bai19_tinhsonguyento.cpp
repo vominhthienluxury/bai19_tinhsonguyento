f33cc5d7ac4c84f0db5babfa73e42916a58eea6f
MThiện CNTT <thanhsooon1111@gmail.com>
2024-11-25 14:25:35 +0700

    Update bai19_tinhsonguyento.cpp
    update3

--- bai19_tinhsonguyento.cpp
+++ bai19_tinhsonguyento.cpp
@@ -3,6 +3,36 @@
 
 #include <iostream>
 using namespace std;
+bool(kiemtraSNT(int s))
+{
+	int dem = 0;
+	for (int i = 1; i <= s; i++)
+	{
+		if (s % i == 0) {
+			dem++;
+		}
+	}
+	if (dem==2)
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+int main() {
+	
+	cout << "KIEM TRA SO CO PHAI SO NGUYEN TO KHONG\n";
+	int so;
+	cout << "nhap so: "; cin >> so;
+		for (int y = 1; y <= so; y++)
+		{
+			if (kiemtraSNT(y) == true) cout << y << " ";
+		
+	}/*
+	for (int i = 1; i <= n; i++)
+	{
+=======
 int main() {
 	int n;
 	int dem = 0;
@@ -22,6 +52,7 @@ int main() {
 	}/*
 	for (int i = 1; i <= n; i++)
 	{
+>>>>>>> 5f9526fb0a18ef0bf6e99be886a6f638ea227379
 		if (n % i == 0) {
 			dem++;
 
